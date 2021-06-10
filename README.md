5bd46c87e8f5cc38f1d395fbc595a7d7fae04619
Stuart Mindt <stuart.mindt@madetech.com>
2021-06-10 14:28:08 +0100

    initial script and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # beacons-etl
+
+## MAC setup
+
+Ensure python 3.5+ is installed
+Ensure Command Line Tools is installed
+
+Install cx_Oracle (Enables connections to oracle instances):
+https://cx-oracle.readthedocs.io/en/latest/user_guide/installation.html
+
+## Running a script
+
+cd to script in terminal
+run python3 scriptnamehere.py
--- push_single_owner_to_api.py
+++ push_single_owner_to_api.py
@@ -1,22 +1,53 @@
+import os
 import cx_Oracle
+import configparser
+import requests
+import datetime
+import time
+
+cx_Oracle.init_oracle_client(
+    lib_dir=os.environ.get("HOME")+"/instantclient_19_8")
+parser = configparser.ConfigParser()
+parser.read('./config/config.ini')
+api_url_owner = parser.get('DEVELOPMENT', 'api_url') + "/person"
+
 
 def _getDBConnection():
     conn = cx_Oracle.connect(
         user="system",
         password="oracle",
-        dsn="localhost/XE")
+        dsn="ec2-18-132-41-196.eu-west-2.compute.amazonaws.com")
     # Set to desired Oracle schema
     conn.current_schema = 'CERSSVD_SCHEMA'
     print("Successfully connected to Oracle Database")
     return conn
 
-connection = _getDBConnection()
-    cursor = connection.cursor()
-    cursor.execute("""
-        begin
-            execute immediate 'drop table BEACON_OWNERS_CLEANED';
-            exception when others then if sqlcode <> -942 then raise; end if;
-        end;""")
-    cursor.execute("""CREATE TABLE BEACON_OWNERS_CLEANED AS SELECT * FROM BEACON_OWNERS WHERE 1=0""")
+
+def _postOwner():
+    conn = _getDBConnection()
+    cursor = conn.cursor()
+    query = cursor.execute("""
+    select * from BEACON_OWNERS where PK_BEACON_OWNER_ID = 24748
+        """)
+    # Get one row from owner and push to test api
+    rows = query.fetchmany()
+
+    if rows == []:
+        print("No results found")
+        return
+
+    print("Sending dummy owner record to Beacons API: ", api_url_owner)
+    for pk_beacon_owner_id, fk_beacon_id, owner_name, company_name, care_of, address_1, address_2, address_3, address_4, country, post_code, phone_1, phone_2, mobile_1, mobile_2, fax, email, is_main, create_user_id, create_dt, update_user_id, update_dt, versioning in rows:
+        jsonObj = {"fullName": owner_name, "email": email, "telephoneNumber": phone_1, "alternativeTelephoneNumber": phone_2, "telephoneNumber2": mobile_1, "alternativeTelephoneNumber2": mobile_2, "addressLine1": address_1,
+                   "addressLine2": address_2, "addressLine3": address_3, "addressLine4": address_4, "townOrCity": address_3, "postcode": post_code, "country": country, "createdDate": time.mktime(create_dt.timetuple()), "updatedDate": time.mktime(update_dt.timetuple())}
+        response = requests.post(api_url_owner, json=jsonObj)
+
+        print("HTTP response status: ", response.status_code)
+        # print("Request: ", response.json())
+
+    conn.commit()
     cursor.close()
-    connection.close()
+    conn.close()
+
+
+_postOwner()
