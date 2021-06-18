15af95f51b085393c4b2ad2148b5c9ecc56fe48d
Stuart Mindt <stuart.mindt@madetech.com>
2021-06-18 16:28:34 +0100

    Adding local config option for local api

--- push_single_owner_to_api.py
+++ push_single_owner_to_api.py
@@ -8,14 +8,14 @@ import config_helper  # noqa
 import legacy_database_helper  # noqa
 
 api_url_owner = config_helper.get_config_parser().get(
-    "DEVELOPMENT", "api_url") + '/person'
+    "LOCAL", "api_url") + '/person'
 
 
 def _postOwner():
     conn = legacy_database_helper.get_db_connection()
     cursor = conn.cursor()
     query = cursor.execute("""
-    select * from BEACON_OWNERS_CLEANED where PK_BEACON_OWNER_ID = 24748
+    select * from BEACON_OWNERS_CLEANED where PK_BEACON_OWNER_ID = 228395
         """)
     # Get one row from owner and push to test api
     rows = query.fetchmany()
@@ -26,9 +26,36 @@ def _postOwner():
 
     print("Sending dummy owner record to Beacons API: ", api_url_owner)
     for pk_beacon_owner_id, fk_beacon_id, owner_name, company_name, care_of, address_1, address_2, address_3, address_4, country, post_code, phone_1, phone_2, mobile_1, mobile_2, fax, email, is_main, create_user_id, create_dt, update_user_id, update_dt, versioning in rows:
-        jsonObj = {"fullName": owner_name, "email": email, "telephoneNumber": phone_1, "alternativeTelephoneNumber": phone_2, "telephoneNumber2": mobile_1, "alternativeTelephoneNumber2": mobile_2, "addressLine1": address_1,
-                   "addressLine2": address_2, "addressLine3": address_3, "addressLine4": address_4, "townOrCity": address_3, "postcode": post_code, "country": country, "createdDate": time.mktime(create_dt.timetuple()), "updatedDate": time.mktime(update_dt.timetuple())}
-        response = requests.post(api_url_owner, json=jsonObj)
+        dataJsonObj = {
+            "data": {
+                "attributes": {
+                    "fullName": owner_name,
+                    "companyName": company_name,
+                    "careOf": care_of,
+                    "email": email,
+                    "telephoneNumber": phone_1,
+                    "alternativeTelephoneNumber": phone_2,
+                    "telephoneNumber2": mobile_1,
+                    "alternativeTelephoneNumber2": mobile_2,
+                    "fax": fax,
+                    "isMain": is_main,
+                    "createUserId": create_user_id,
+                    "updateUserId": update_user_id,
+                    "addressLine1": address_1,
+                    "addressLine2": address_2,
+                    "addressLine3": address_3,
+                    "addressLine4": address_4,
+                    "townOrCity": address_3,
+                    "postcode": post_code,
+                    "country": country,
+                    "createdDate": str(create_dt),
+                    "LastModifiedDate": str(update_dt),
+                    "versioning": versioning,
+                    "personType": "OWNER"
+                }
+            }
+        }
+        response = requests.post(api_url_owner, json=dataJsonObj)
 
         print("HTTP response status: ", response.status_code)
         # print("Request: ", response.json())
