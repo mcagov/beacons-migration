8568046054b5633cabb6f9dc84d8f328509f742c
Matthew Carr <matt.carr@madetech.com>
2021-06-30 18:25:15 +0100

    Add test for aggregating user with all information

--- tests/test_aggregate_owners.py
+++ tests/test_aggregate_owners.py
@@ -24,7 +24,6 @@ create_dt, ❌
 update_user_id, ❌
 update_dt, ❌
 versioning ❌
-
 """
 from etl.aggregate_owners import aggregate_owners
 
@@ -109,6 +108,68 @@ def test_no_users_aggregated():
                        'mobile_1': '07713812678',
                        'mobile_2': None,
                        'fax': None,
+                       'email': 'mca@mcga.gov.uk'
+                   }
+               }
+           ]
+
+
+def test_two_users_aggregated():
+    assert aggregate_owners([
+        {
+            'pk_beacon_owner_id': 1,
+            'owner_name': 'Matt',
+            'company_name': 'MCA',
+            'care_of': 'MCA',
+            'address_1': "10 City Beach",
+            'address_2': 'Salt Lake',
+            'address_3': 'At Home',
+            'address_4': 'In Bed',
+            'country': 'UK',
+            'post_code': 'CL1 2DG',
+            'phone_1': '0117123456',
+            'phone_2': 'Call me',
+            'mobile_1': '07713812678',
+            'mobile_2': 'On my mobile',
+            'fax': 'Fax me',
+            'email': 'mca@mcga.gov.uk'
+        },
+        {
+            'pk_beacon_owner_id': 2,
+            'owner_name': 'Matt',
+            'company_name': 'MCA',
+            'care_of': 'MCA',
+            'address_1': "10 City Beach",
+            'address_2': 'Salt Lake',
+            'address_3': 'At Home',
+            'address_4': 'In Bed',
+            'country': 'UK',
+            'post_code': 'CL1 2DG',
+            'phone_1': '0117123456',
+            'phone_2': 'Call me',
+            'mobile_1': '07713812678',
+            'mobile_2': 'On my mobile',
+            'fax': 'Fax me',
+            'email': 'mca@mcga.gov.uk'
+        }
+    ]) == [
+               {
+                   'pk_keys': {1, 2},
+                   'owner': {
+                       'owner_name': 'Matt',
+                       'company_name': 'MCA',
+                       'care_of': 'MCA',
+                       'address_1': "10 City Beach",
+                       'address_2': 'Salt Lake',
+                       'address_3': 'At Home',
+                       'address_4': 'In Bed',
+                       'country': 'UK',
+                       'post_code': 'CL1 2DG',
+                       'phone_1': '0117123456',
+                       'phone_2': 'Call me',
+                       'mobile_1': '07713812678',
+                       'mobile_2': 'On my mobile',
+                       'fax': 'Fax me',
                        'email': 'mca@mcga.gov.uk'
                    }
                }
