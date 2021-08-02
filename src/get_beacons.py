6a9f11f444a4ef5fc82b0793291c580defb28127
Matthew Carr <matt.carr@madetech.com>
2021-08-02 10:50:24 +0100

    Conform to PEP8 requirements

--- src/get_beacons.py
+++ src/get_beacons.py
@@ -9,19 +9,19 @@ cursor = db_connection.cursor()
 
 GET_ALL_BEACONS_QUERY = "SELECT * FROM BEACONS"
 
+
 def get_beacons():
     results = []
 
     beacons = _get_beacon_rows()
-    
+
     for pk_beacon_id, status_code, is_withdrawn, is_pending, depart_ref_id, \
         hex_id, serial_no, cospas_sarsat_no, fk_mti_code, fk_csta_code, \
         fk_model_code, fk_protocol_type_id, fk_beacon_type_code, \
         fk_manufacturer_id, manufacturer_serial_no, coding, \
         first_registration_dt, last_service_date, battery_expiry_date, \
         emergency_contact, withdrawn_reason, note, is_archived, create_user_id, \
-        create_dt, update_user_id, update_dt, versioning,  in beacons:
-
+        create_dt, update_user_id, update_dt, versioning, in beacons:
         uses = get_uses(pk_beacon_id)
         owners = get_owners(pk_beacon_id)
 
@@ -60,14 +60,16 @@ def get_beacons():
 
     return results
 
+
 def _get_beacon_rows():
     cursor.execute(GET_ALL_BEACONS_QUERY)
     return cursor.fetchmany(5)
     # TODO: Get all beacons
     # return cursor.fetchall()
 
+
 def _get_beacon_manufacturer(manufacturer_id):
-    if (manufacturer_id is None):
+    if manufacturer_id is None:
         return manufacturer_id
     else:
         query = cursor.execute(f"""
@@ -75,13 +77,14 @@ def _get_beacon_manufacturer(manufacturer_id):
         FROM TREF_BEACON_MANUFACTURERS
         WHERE PK_ID='{manufacturer_id}'
         """)
-        
+
         manufacturer_row = query.fetchone()
 
         return manufacturer_row[0]
 
+
 def _get_beacon_type(beacon_type_id):
-    if (beacon_type_id is None):
+    if beacon_type_id is None:
         return beacon_type_id
     else:
         query = cursor.execute(f"""
@@ -89,13 +92,14 @@ def _get_beacon_type(beacon_type_id):
         FROM TREF_BEACON_TYPES
         WHERE PK_CODE='{beacon_type_id}'
         """)
-        
+
         beacon_type_row = query.fetchone()
 
         return beacon_type_row[0]
 
+
 def _get_beacon_model(model_id):
-    if (model_id is None):
+    if model_id is None:
         return model_id
     else:
         query = cursor.execute(f"""
@@ -103,13 +107,14 @@ def _get_beacon_model(model_id):
         FROM TREF_BEACON_MODELS
         WHERE PK_CODE='{model_id}'
         """)
-    
+
         model_row = query.fetchone()
 
         return model_row[0]
 
+
 def _get_beacon_protocol(protocol_id):
-    if (protocol_id is None):
+    if protocol_id is None:
         return protocol_id
     else:
         query = cursor.execute(f"""
@@ -117,7 +122,7 @@ def _get_beacon_protocol(protocol_id):
         FROM TREF_BEACON_PROTOCOLS
         WHERE PK_ID='{protocol_id}'
         """)
-    
+
         protocol_row = query.fetchone()
 
         return protocol_row[0]
