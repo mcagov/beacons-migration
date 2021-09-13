6e395062e41aa12911d04ca97a4434a6974314c1
Ninamma Rai <32230328+Ninamma@users.noreply.github.com>
2021-09-13 16:04:09 +0100

    Remove filtering phone numbers - to prevent data loss

--- src/owner_cleansing_rules.py
+++ src/owner_cleansing_rules.py
@@ -7,8 +7,6 @@ db_connection = legacy_database_helper.get_db_connection()
 cursor = db_connection.cursor()
 
 email_regex = '[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}'
-phone_regex = '[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*'
-mobile_regex = '^(\+44\s?7\d{3}|\(?07\d{3}\)?)\s?\d{3}\s?\d{3}$'
 postcode_regex = '([A-Z][A-HJ-Y]?\d[A-Z\d]? ?\d[A-Z]{2}|GIR ?0A{2})'
 broken_countries = {}
 
@@ -44,23 +42,6 @@ def _get_valid_emails_list_from_fields(email, phone_1, phone_2, mobile_1, mobile
     return valid_emails
 
 
-def _get_valid_phone_numbers_list_from_fields(email, phone_1, phone_2, mobile_1, mobile_2):
-    valid_phone_numbers = []
-
-    if (email != None):
-        valid_phone_numbers.extend(re.findall(phone_regex, email))
-    if (phone_1 != None):
-        valid_phone_numbers.extend(re.findall(phone_regex, phone_1))
-    if (phone_2 != None):
-        valid_phone_numbers.extend(re.findall(phone_regex, phone_2))
-    if (mobile_1 != None):
-        valid_phone_numbers.extend(re.findall(phone_regex, mobile_1))
-    if (mobile_2 != None):
-        valid_phone_numbers.extend(re.findall(phone_regex, mobile_2))
-
-    return valid_phone_numbers
-
-
 def _get_valid_uk_postcode_list_from_fields(address_1, address_2, address_3, address_4, post_code, country):
     uk_postcodes = []
 
@@ -78,26 +59,6 @@ def _get_valid_uk_postcode_list_from_fields(address_1, address_2, address_3, add
         uk_postcodes.extend(re.findall(postcode_regex, country))
 
     return uk_postcodes
-
-
-def _get_valid_uk_mobile_list_from_valid_phone_list(valid_phone_numbers):
-    uk_mobiles = []
-
-    for phone_number in valid_phone_numbers:
-        if re.fullmatch(mobile_regex, phone_number):
-            uk_mobiles.append(phone_number)
-
-    return uk_mobiles
-
-
-def _get_valid_phone_number_list_from_valid_phone_list(valid_phone_numbers):
-    phone_numbers = []
-
-    for phone_number in valid_phone_numbers:
-        if re.fullmatch(phone_regex, phone_number):
-            phone_numbers.append(phone_number)
-
-    return phone_numbers
 
 
 def _extract_by_regex(regex, field):
@@ -151,30 +112,6 @@ def _set_country(country, valid_country):
     return country
 
 
-def _set_phone_1(phone_1, other_phone_numbers):
-    if len(other_phone_numbers) > 0:
-        phone_1 = other_phone_numbers[0]
-    return phone_1
-
-
-def _set_phone_2(phone_2, other_phone_numbers):
-    if len(other_phone_numbers) > 1:
-        phone_2 = other_phone_numbers[1]
-    return phone_2
-
-
-def _set_mobile_1(mobile_1, uk_mobiles):
-    if len(uk_mobiles) > 0:
-        mobile_1 = uk_mobiles[0]
-    return mobile_1
-
-
-def _set_mobile_2(phone_2, uk_mobiles):
-    if len(uk_mobiles) > 1:
-        phone_2 = uk_mobiles[1]
-    return phone_2
-
-
 def _set_email(email, valid_emails):
     if len(valid_emails) > 0:
         email = valid_emails[0].lower()
@@ -188,19 +125,6 @@ def _cleanse_owner_row(pk_beacon_owner_id, fk_beacon_id, owner_name, company_nam
         email, phone_1, phone_2, mobile_1, mobile_1)
 
     email = _extract_by_regex(email_regex, email)
-    phone_1 = _extract_by_regex(email_regex, phone_1)
-    phone_2 = _extract_by_regex(email_regex, phone_2)
-    mobile_1 = _extract_by_regex(email_regex, mobile_1)
-    mobile_2 = _extract_by_regex(email_regex, mobile_2)
-
-    valid_phone_numbers = _get_valid_phone_numbers_list_from_fields(
-        email, phone_1, phone_2, mobile_1, mobile_1)
-
-    email = _extract_by_regex(phone_regex, email)
-    phone_1 = _extract_by_regex(phone_regex, phone_1)
-    phone_2 = _extract_by_regex(phone_regex, phone_2)
-    mobile_1 = _extract_by_regex(phone_regex, mobile_1)
-    mobile_2 = _extract_by_regex(phone_regex, mobile_2)
 
     valid_country = _get_valid_country_from_fields(
         address_1, address_2, address_3, address_4, post_code, country)
@@ -210,20 +134,10 @@ def _cleanse_owner_row(pk_beacon_owner_id, fk_beacon_id, owner_name, company_nam
         valid_uk_postcodes = _get_valid_uk_postcode_list_from_fields(
             address_1, address_2, address_3, address_4, post_code, country)
 
-    uk_mobiles = _get_valid_uk_mobile_list_from_valid_phone_list(
-        valid_phone_numbers)
-
-    other_phone_numbers = _get_valid_phone_number_list_from_valid_phone_list(
-        valid_phone_numbers)
-
     # Start setting new values
 
     post_code = _set_postcode(post_code, valid_uk_postcodes)
     country = _set_country(country, valid_country)
-    phone_1 = _set_phone_1(phone_1, other_phone_numbers)
-    phone_2 = _set_phone_2(phone_2, other_phone_numbers)
-    mobile_1 = _set_mobile_1(mobile_1, uk_mobiles)
-    mobile_2 = _set_mobile_2(mobile_2, uk_mobiles)
     email = _set_email(email, valid_emails)
 
     # Insert record into DB
